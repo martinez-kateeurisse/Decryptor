d10a4d7ce28644094046cb7174d8d4206c914361
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-15 19:24:11 +0800

    Pseudocode

---
+++ decryption.py
@@ -0,0 +1,14 @@
+#DECRYPTION
+#A Python Script that accepts a string as encrypted text and then the program will decrypt it using the following character substitute:
+#'a' = *, 'e' = & , 'i' = # , 'o' = + 'u' = !
+
+#Ask the user to input an encrypted text
+#Check each character of the user's input
+#If '*', change to 'a'
+#If '&', change to 'e'
+#If '#', change to 'i'
+#If '+', change to 'o'
+#If '!', change to 'u'
+
+#Print the output
+
